50d6ea11e6e46baa51b6039250604c0ea7a497ac
Tmuat <Thomas_Muat@hotmail.com>
2021-05-06 16:35:15 +0000

    CORE: Black formatting

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,47 +1,62 @@
 from two_factor.views import (
-    BackupTokensView, ProfileView, QRGeneratorView, SetupCompleteView,
+    BackupTokensView,
+    ProfileView,
+    QRGeneratorView,
+    SetupCompleteView,
 )
 
 from django.contrib.auth import views as auth_views
 from django.urls import path
 
 from accounts.forms import CustomOTPAuthenticationForm
-from accounts.views import logout_modal, otp_remove, SetupView, otp_setup
+from accounts.views import (
+    logout_modal,
+    otp_remove,
+    otp_setup,
+    otp_backup,
+    SetupView,
+)
 
 
 urlpatterns = [
-    path("login/", auth_views.LoginView.as_view(authentication_form=CustomOTPAuthenticationForm), name="login"),
+    path(
+        "login/",
+        auth_views.LoginView.as_view(
+            authentication_form=CustomOTPAuthenticationForm
+        ),
+        name="login",
+    ),
     path("logout/", auth_views.LogoutView.as_view(), name="logout"),
     path("logout-modal/", logout_modal, name="logout_modal"),
     path(
-        'two-factor/profile/',
+        "two-factor/profile/",
         ProfileView.as_view(),
-        name='profile',
+        name="tfa-profile",
     ),
     path(
-        'two-factor/qrcode/',
+        "two-factor/qrcode/",
         QRGeneratorView.as_view(),
-        name='qr',
+        name="qr",
     ),
     path(
-        'two-factor/setup/complete/',
+        "two-factor/setup/complete/",
         SetupCompleteView.as_view(),
-        name='setup_complete',
+        name="setup_complete",
     ),
     path(
-        'two-factor/backup/tokens/',
-        BackupTokensView.as_view(),
-        name='backup_tokens',
-    ),
-    path(
-        'two-factor/remove/',
+        "two-factor/remove/",
         otp_remove,
-        name='otp_remove',
+        name="otp_remove",
     ),
     path(
-        'two-factor/setup/',
+        "two-factor/setup/",
         otp_setup,
-        name='setup',
+        name="setup",
     ),
-    path('test/', otp_setup, name="test")
+    path(
+        "two-factor/backup/tokens/",
+        otp_backup,
+        name="backup_tokens",
+    ),
+    path("test/", otp_setup, name="test"),
 ]
