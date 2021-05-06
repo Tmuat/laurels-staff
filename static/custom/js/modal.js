d7192f9080d9f8b548f77d4a9655d2cda637bfcd
Tmuat <Thomas_Muat@hotmail.com>
2021-05-06 13:04:06 +0000

    MODAL: Added jquery ready function

--- static/custom/js/modal.js
+++ static/custom/js/modal.js
@@ -1,17 +1,15 @@
-$(function () {
-
-    $(".js-logout").click(function () {
-      $.ajax({
-        url: '/account/logout-modal/',
-        type: 'get',
-        dataType: 'json',
-        beforeSend: function () {
-          $("#base-modal").modal("show");
-        },
-        success: function (data) {
-          $("#base-modal .modal-dialog").html(data.html_modal);
-        }
-      });
+$(document).ready(function(){
+  $(".js-logout").click(function () {
+    $.ajax({
+      url: '/account/logout-modal/',
+      type: 'get',
+      dataType: 'json',
+      beforeSend: function () {
+        $("#base-modal").modal("show");
+      },
+      success: function (data) {
+        $("#base-modal .modal-dialog").html(data.html_modal);
+      }
     });
-  
   });
+});
