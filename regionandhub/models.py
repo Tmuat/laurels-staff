e86788f77ec469d0c1b0c2871d30a89b363b0540
Tmuat <Thomas_Muat@hotmail.com>
2021-05-08 11:51:15 +0000

    REGION&HUB: Created region model

--- regionandhub/models.py
+++ regionandhub/models.py
@@ -1,3 +1,21 @@
 from django.db import models
 
-# Create your models here.
+from common.models import UpdatedAndCreated
+
+
+class Region(UpdatedAndCreated):
+    class Meta:
+        ordering = ["name"]
+        verbose_name_plural = "Regions"
+
+    name = models.CharField(
+        max_length=50, null=False, blank=False, unique=True
+    )
+    slug = models.SlugField(null=False, unique=True)
+    is_active = models.BooleanField(default=True)
+
+    def __str__(self):
+        return self.name
+
+    def get_slug(self):
+        return self.slug
