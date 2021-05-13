b60d85c5155b9582a5b4e73ce75bf7e3d2e6a042
Tmuat <Thomas_Muat@hotmail.com>
2021-05-13 12:31:45 +0000

    USERS: Created basic employee view

--- users/urls.py
+++ users/urls.py
@@ -1,4 +1,7 @@
 from django.urls import path, include
 
+from users.views import EmployeeListView
 
-urlpatterns = []
+
+app_name = "users"
+urlpatterns = [path("", EmployeeListView.as_view(), name="employees")]
--- users/views.py
+++ users/views.py
@@ -1,3 +1,14 @@
-from django.shortcuts import render
+from django_otp.decorators import otp_required
 
-# Create your views here.
+from django.contrib.auth import get_user_model
+from django.contrib.auth.decorators import login_required
+from django.shortcuts import render
+from django.views.generic.list import ListView
+
+
+class EmployeeListView(ListView):
+
+    # model = get_user_model()
+    queryset = get_user_model().objects.all()
+    paginate_by = 100  # if pagination is desired
+    template_name = "users/employee_list_view.html"
