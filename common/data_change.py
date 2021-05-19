91db81f40cc9b56113963e01caa6f2071c85075f
Tmuat <Thomas_Muat@hotmail.com>
2021-05-19 14:14:47 +0000

    CORE: Added sentry to requirements.txt

--- common/data_change.py
+++ common/data_change.py
@@ -1,220 +1,229 @@
 import json
 import uuid
 
+from django.template.defaultfilters import slugify
+
 
 # exec(open('common/data_change.py').read())
 
+# Property Model
+
 with open(
-    "/workspace/laurels-staff/common/data_dump/property.json", "r"
+    "/workspace/laurels-staff/common/data_dump/originals/property.json", "r"
 ) as json_data:
     property_model = json.load(json_data)  # deserialises it
     # data2 = json.dumps(data1) # json formatted string
 
-for prop in property_model:
+for instance in property_model:
 
     # Changing the model to new value
 
-    prop["model"] = "properties.property"
+    instance["model"] = "properties.property"
 
     # End changing the model to new value
 
     # Changing the property type to new values
 
-    if prop["fields"]["property_type"] == "House - Terraced":
-        prop["fields"]["property_type"] = "house_terraced"
+    if instance["fields"]["property_type"] == "House - Terraced":
+        instance["fields"]["property_type"] = "house_terraced"
 
-    elif prop["fields"]["property_type"] == "House - End of Terrace":
-        prop["fields"]["property_type"] = "house_end_terrace"
+    elif instance["fields"]["property_type"] == "House - End of Terrace":
+        instance["fields"]["property_type"] = "house_end_terrace"
 
-    elif prop["fields"]["property_type"] == "House - Semi-Detached":
-        prop["fields"]["property_type"] = "house_semi_detached"
+    elif instance["fields"]["property_type"] == "House - Semi-Detached":
+        instance["fields"]["property_type"] = "house_semi_detached"
 
-    elif prop["fields"]["property_type"] == "House - Detached":
-        prop["fields"]["property_type"] = "house_detached"
+    elif instance["fields"]["property_type"] == "House - Detached":
+        instance["fields"]["property_type"] = "house_detached"
 
-    elif prop["fields"]["property_type"] == "Maisonette - Ground Floor":
-        prop["fields"]["property_type"] = "maisonette_ground_floor"
+    elif instance["fields"]["property_type"] == "Maisonette - Ground Floor":
+        instance["fields"]["property_type"] = "maisonette_ground_floor"
 
-    elif prop["fields"]["property_type"] == "Maisonette - Top Floor":
-        prop["fields"]["property_type"] = "maisonette_top_floor"
+    elif instance["fields"]["property_type"] == "Maisonette - Top Floor":
+        instance["fields"]["property_type"] = "maisonette_top_floor"
 
-    elif prop["fields"]["property_type"] == "Flat - Ground Floor":
-        prop["fields"]["property_type"] = "flat_ground_floor"
+    elif instance["fields"]["property_type"] == "Flat - Ground Floor":
+        instance["fields"]["property_type"] = "flat_ground_floor"
 
-    elif prop["fields"]["property_type"] == "Flat - Upper Floors":
-        prop["fields"]["property_type"] = "flat_upper_floor"
+    elif instance["fields"]["property_type"] == "Flat - Upper Floors":
+        instance["fields"]["property_type"] = "flat_upper_floor"
 
-    elif prop["fields"]["property_type"] == "Bungalow - Semi-Detached":
-        prop["fields"]["property_type"] = "bungalow_semi_detached"
+    elif instance["fields"]["property_type"] == "Bungalow - Semi-Detached":
+        instance["fields"]["property_type"] = "bungalow_semi_detached"
 
-    elif prop["fields"]["property_type"] == "Bungalow - Detached":
-        prop["fields"]["property_type"] = "bungalow_detached"
+    elif instance["fields"]["property_type"] == "Bungalow - Detached":
+        instance["fields"]["property_type"] = "bungalow_detached"
 
-    elif prop["fields"]["property_type"] == "Commercial":
-        prop["fields"]["property_type"] = "commercial"
+    elif instance["fields"]["property_type"] == "Commercial":
+        instance["fields"]["property_type"] = "commercial"
 
-    elif prop["fields"]["property_type"] == "Land":
-        prop["fields"]["property_type"] = "land"
+    elif instance["fields"]["property_type"] == "Land":
+        instance["fields"]["property_type"] = "land"
 
-    elif prop["fields"]["property_type"] == "Other":
-        prop["fields"]["property_type"] = "other"
+    elif instance["fields"]["property_type"] == "Other":
+        instance["fields"]["property_type"] = "other"
 
     # End change property type
 
     # Changing the property style to new values
 
-    if prop["fields"]["property_style"] == "Modern":
-        prop["fields"]["property_style"] = "modern"
+    if instance["fields"]["property_style"] == "Modern":
+        instance["fields"]["property_style"] = "modern"
 
-    elif prop["fields"]["property_style"] == "New Build":
-        prop["fields"]["property_style"] = "new_build"
+    elif instance["fields"]["property_style"] == "New Build":
+        instance["fields"]["property_style"] = "new_build"
 
-    elif prop["fields"]["property_style"] == "Period":
-        prop["fields"]["property_style"] = "period"
+    elif instance["fields"]["property_style"] == "Period":
+        instance["fields"]["property_style"] = "period"
 
     # End change property style
 
     # Changing the property tenure to new values
 
-    if prop["fields"]["tenure"] == "Freehold":
-        prop["fields"]["tenure"] = "freehold"
+    if instance["fields"]["tenure"] == "Freehold":
+        instance["fields"]["tenure"] = "freehold"
 
-    elif prop["fields"]["tenure"] == "Leasehold":
-        prop["fields"]["tenure"] = "leasehold"
+    elif instance["fields"]["tenure"] == "Leasehold":
+        instance["fields"]["tenure"] = "leasehold"
 
-    elif prop["fields"]["tenure"] == "Share of Freehold":
-        prop["fields"]["tenure"] = "share_of_freehold"
+    elif instance["fields"]["tenure"] == "Share of Freehold":
+        instance["fields"]["tenure"] = "share_of_freehold"
 
     # End change property tenure
 
     # Add new fields
 
-    prop["fields"]["created_by"] = "Admin"
-    prop["fields"]["created"] = "2000-01-13T13:13:13.000Z"
-    prop["fields"]["updated_by"] = "Admin"
-    prop["fields"]["updated"] = "2000-01-13T13:13:13.000Z"
+    instance["fields"]["created_by"] = "Admin"
+    instance["fields"]["created"] = "2000-01-13T13:13:13.000Z"
+    instance["fields"]["updated_by"] = "Admin"
+    instance["fields"]["updated"] = "2000-01-13T13:13:13.000Z"
 
     # End add new fields
 
     # Move original PK
 
-    prop["old_pk"] = prop["pk"]
+    instance["old_pk"] = instance["pk"]
 
     # End move original PK
 
     # Create new UUID field
 
-    prop["pk"] = str(uuid.uuid4())
+    instance["pk"] = str(uuid.uuid4())
 
 with open(
     "/workspace/laurels-staff/common/data_dump/property.json", "w"
 ) as json_data:
     json.dump(property_model, json_data)
 
+# End Property Model
+
+# Remeber to add Region UUID
+# Hub Model
 
 with open(
-    "/workspace/laurels-staff/common/data_dump/property_process.json", "r"
+    "/workspace/laurels-staff/common/data_dump/originals/hub.json", "r"
 ) as json_data:
-    property_process_model = json.load(json_data)  # deserialises it
-    # data2 = json.dumps(data1) # json formatted string
+    hub_model = json.load(json_data)
 
-for prop in property_process_model:
+for instance in hub_model:
 
     # Changing the model to new value
 
-    prop["model"] = "properties.property"
+    instance["model"] = "regionandhub.hub"
 
     # End changing the model to new value
 
-    # Changing the property type to new values
+    # Add/Update fields
 
-    if prop["fields"]["property_type"] == "House - Terraced":
-        prop["fields"]["property_type"] = "house_terraced"
+    hub_slug = slugify(instance["fields"]["hub_name"])
+    
+    instance["fields"]["slug"] = hub_slug
 
-    elif prop["fields"]["property_type"] == "House - End of Terrace":
-        prop["fields"]["property_type"] = "house_end_terrace"
+    instance["fields"]["is_active"] = True
 
-    elif prop["fields"]["property_type"] == "House - Semi-Detached":
-        prop["fields"]["property_type"] = "house_semi_detached"
+    instance["fields"]["region"] = "f26b0560-01d7-45af-9ae0-5e3d7e388829"
 
-    elif prop["fields"]["property_type"] == "House - Detached":
-        prop["fields"]["property_type"] = "house_detached"
+    del instance["fields"]["hub_targets"]
 
-    elif prop["fields"]["property_type"] == "Maisonette - Ground Floor":
-        prop["fields"]["property_type"] = "maisonette_ground_floor"
-
-    elif prop["fields"]["property_type"] == "Maisonette - Top Floor":
-        prop["fields"]["property_type"] = "maisonette_top_floor"
-
-    elif prop["fields"]["property_type"] == "Flat - Ground Floor":
-        prop["fields"]["property_type"] = "flat_ground_floor"
-
-    elif prop["fields"]["property_type"] == "Flat - Upper Floors":
-        prop["fields"]["property_type"] = "flat_upper_floor"
-
-    elif prop["fields"]["property_type"] == "Bungalow - Semi-Detached":
-        prop["fields"]["property_type"] = "bungalow_semi_detached"
-
-    elif prop["fields"]["property_type"] == "Bungalow - Detached":
-        prop["fields"]["property_type"] = "bungalow_detached"
-
-    elif prop["fields"]["property_type"] == "Commercial":
-        prop["fields"]["property_type"] = "commercial"
-
-    elif prop["fields"]["property_type"] == "Land":
-        prop["fields"]["property_type"] = "land"
-
-    elif prop["fields"]["property_type"] == "Other":
-        prop["fields"]["property_type"] = "other"
-
-    # End change property type
-
-    # Changing the property style to new values
-
-    if prop["fields"]["property_style"] == "Modern":
-        prop["fields"]["property_style"] = "modern"
-
-    elif prop["fields"]["property_style"] == "New Build":
-        prop["fields"]["property_style"] = "new_build"
-
-    elif prop["fields"]["property_style"] == "Period":
-        prop["fields"]["property_style"] = "period"
-
-    # End change property style
-
-    # Changing the property tenure to new values
-
-    if prop["fields"]["tenure"] == "Freehold":
-        prop["fields"]["tenure"] = "freehold"
-
-    elif prop["fields"]["tenure"] == "Leasehold":
-        prop["fields"]["tenure"] = "leasehold"
-
-    elif prop["fields"]["tenure"] == "Share of Freehold":
-        prop["fields"]["tenure"] = "share_of_freehold"
-
-    # End change property tenure
+    # End Add/Update fields
 
     # Add new fields
 
-    prop["fields"]["created_by"] = "Admin"
-    prop["fields"]["created"] = "2000-01-13T13:13:13.000Z"
-    prop["fields"]["updated_by"] = "Admin"
-    prop["fields"]["updated"] = "2000-01-13T13:13:13.000Z"
+    instance["fields"]["created_by"] = "Admin"
+    instance["fields"]["created"] = "2000-01-13T13:13:13.000Z"
+    instance["fields"]["updated_by"] = "Admin"
+    instance["fields"]["updated"] = "2000-01-13T13:13:13.000Z"
 
     # End add new fields
 
     # Move original PK
 
-    prop["old_pk"] = prop["pk"]
+    instance["old_pk"] = instance["pk"]
 
     # End move original PK
 
     # Create new UUID field
 
-    prop["pk"] = str(uuid.uuid4())
+    instance["pk"] = str(uuid.uuid4())
 
 with open(
-    "/workspace/laurels-staff/common/data_dump/property.json", "w"
+    "/workspace/laurels-staff/common/data_dump/hub.json", "w"
 ) as json_data:
-    json.dump(data1, json_data)
+    json.dump(hub_model, json_data)
+
+# End Hub Model
+
+# Custom User Model
+
+with open(
+    "/workspace/laurels-staff/common/data_dump/originals/users.json", "r"
+) as json_data:
+    custom_user_model = json.load(json_data)
+
+for instance in custom_user_model:
+
+    # Changing the model to new value
+
+    instance["model"] = "users.customuser"
+
+    # End changing the model to new value
+
+    # Add/Update fields
+
+    hub_slug = slugify(instance["fields"]["hub_name"])
+    
+    instance["fields"]["slug"] = hub_slug
+
+    instance["fields"]["is_active"] = True
+
+    instance["fields"]["region"] = "f26b0560-01d7-45af-9ae0-5e3d7e388829"
+
+    del instance["fields"]["hub_targets"]
+
+    # End Add/Update fields
+
+    # Add new fields
+
+    instance["fields"]["created_by"] = "Admin"
+    instance["fields"]["created"] = "2000-01-13T13:13:13.000Z"
+    instance["fields"]["updated_by"] = "Admin"
+    instance["fields"]["updated"] = "2000-01-13T13:13:13.000Z"
+
+    # End add new fields
+
+    # Move original PK
+
+    instance["old_pk"] = instance["pk"]
+
+    # End move original PK
+
+    # Create new UUID field
+
+    instance["pk"] = str(uuid.uuid4())
+
+with open(
+    "/workspace/laurels-staff/common/data_dump/hub.json", "w"
+) as json_data:
+    json.dump(custom_user_model, json_data)
+
+# End Custom User Model
