20d4fa87c6100fed46a81be1fed21b37a24985c7
Tmuat <Thomas_Muat@hotmail.com>
2021-05-20 14:31:42 +0000

    COMMON: More data change script

--- common/data_change.py
+++ common/data_change.py
@@ -9,7 +9,9 @@ from django.template.defaultfilters import slugify
 
 # exec(open('common/data_change.py').read())
 
+# python3 manage.py dumpdata home.propertyprocess > property_process.json
 # python3 manage.py dumpdata otp_totp.totpdevice > totp.json
+# python3 manage.py loaddata master.json
 
 # ------------------------------------------------------------------------------
 # FUNCITONS
@@ -30,16 +32,38 @@ from django.template.defaultfilters import slugify
 # DICTIONARIES
 # ------------------------------------------------------------------------------
 
+setup_dict = None
+super_dict = None
 property_dict = None
 propertylink_dict = None
 hub_dict = None
+hub_targets_dict = None
 user_dict = None
+totp_dict = None
 profile_dict = None
 master_dict = None
 
 # ------------------------------------------------------------------------------
 # START SCRIPT
 # ------------------------------------------------------------------------------
+
+# ----------------------------------------
+# SETUP
+# ----------------------------------------
+
+with open(
+    "/workspace/laurels-staff/common/data_dump/originals/setup.json", "r"
+) as json_data:
+    setup = json.load(json_data)
+
+setup_dict = setup
+
+with open(
+    "/workspace/laurels-staff/common/data_dump/originals/superuser.json", "r"
+) as json_data:
+    super = json.load(json_data)
+
+super_dict = super
 
 # ----------------------------------------
 # PROPERTY MODEL
@@ -180,6 +204,8 @@ for instance in hub_model:
 
     instance["fields"]["region"] = "aa8b84a7-8cd0-464a-83f1-ceea7b094e73"
 
+    instance["hub_targets_link"] = instance["fields"]["hub_targets"]
+
     del instance["fields"]["hub_targets"]
 
     # End Add/Update fields
@@ -209,6 +235,194 @@ with open(
     "/workspace/laurels-staff/common/data_dump/hub.json", "w"
 ) as json_data:
     json.dump(hub_model, json_data)
+
+
+# ----------------------------------------
+# HUB TARGETS MODEL
+# ----------------------------------------
+
+with open(
+    "/workspace/laurels-staff/common/data_dump/originals/hubtargets.json", "r"
+) as json_data:
+    hub_targets_model = json.load(json_data)
+
+hub_targets = []
+
+for instance in hub_targets_model:
+
+    q1_2021 = {}
+    q2_2021 = {}
+    q3_2021 = {}
+    q4_2021 = {}
+
+    q1_2021_fields = {}
+    q2_2021_fields = {}
+    q3_2021_fields = {}
+    q4_2021_fields = {}
+
+    hub_targets_year = {}
+    hub_targets_year_fields = {}
+
+    # Changing the model to new value
+
+    q1_2021["model"] = "regionandhub.hubtargets"
+    q2_2021["model"] = "regionandhub.hubtargets"
+    q3_2021["model"] = "regionandhub.hubtargets"
+    q4_2021["model"] = "regionandhub.hubtargets"
+
+    hub_targets_year["model"] = "regionandhub.hubtargetsyear"
+
+    # End changing the model to new value
+
+    # Create pk for new instances
+
+    q1_2021["pk"] = str(uuid.uuid4())
+    q2_2021["pk"] = str(uuid.uuid4())
+    q3_2021["pk"] = str(uuid.uuid4())
+    q4_2021["pk"] = str(uuid.uuid4())
+
+    hub_targets_year["pk"] = str(uuid.uuid4())
+
+    # End creating new instances
+
+    # Adding year & quarter for new instances
+
+    q1_2021_fields["year"] = "2021"
+    q2_2021_fields["year"] = "2021"
+    q3_2021_fields["year"] = "2021"
+    q4_2021_fields["year"] = "2021"
+
+    hub_targets_year_fields["year"] = "2021"
+
+    q1_2021_fields["quarter"] = "q1"
+    q2_2021_fields["quarter"] = "q2"
+    q3_2021_fields["quarter"] = "q3"
+    q4_2021_fields["quarter"] = "q4"
+
+    # End adding year & quarter new instances
+
+    # Adding hub link
+
+    for hub_instance in hub_dict:
+        if hub_instance["hub_targets_link"] == instance["pk"]:
+
+            q1_2021_fields["hub_targets"] = hub_instance["pk"]
+            q2_2021_fields["hub_targets"] = hub_instance["pk"]
+            q3_2021_fields["hub_targets"] = hub_instance["pk"]
+            q4_2021_fields["hub_targets"] = hub_instance["pk"]
+
+            hub_targets_year_fields["hub"] = hub_instance["pk"]
+
+    # End hub link
+
+    # Set hub targets to true
+
+    hub_targets_year_fields["targets_set"] = True
+
+    # End set hub targets to true
+
+    # Update fields
+
+    q1_2021_fields["instructions"] = instance["fields"][
+        "q1_2021_instructions_hub"
+    ]
+    q2_2021_fields["instructions"] = instance["fields"][
+        "q2_2021_instructions_hub"
+    ]
+    q3_2021_fields["instructions"] = instance["fields"][
+        "q3_2021_instructions_hub"
+    ]
+    q4_2021_fields["instructions"] = instance["fields"][
+        "q4_2021_instructions_hub"
+    ]
+
+    q1_2021_fields["reductions"] = instance["fields"]["q1_2021_reductions_hub"]
+    q2_2021_fields["reductions"] = instance["fields"]["q2_2021_reductions_hub"]
+    q3_2021_fields["reductions"] = instance["fields"]["q3_2021_reductions_hub"]
+    q4_2021_fields["reductions"] = instance["fields"]["q4_2021_reductions_hub"]
+
+    q1_2021_fields["new_business"] = instance["fields"][
+        "q1_2021_new_business_hub"
+    ]
+    q2_2021_fields["new_business"] = instance["fields"][
+        "q2_2021_new_business_hub"
+    ]
+    q3_2021_fields["new_business"] = instance["fields"][
+        "q3_2021_new_business_hub"
+    ]
+    q4_2021_fields["new_business"] = instance["fields"][
+        "q4_2021_new_business_hub"
+    ]
+
+    q1_2021_fields["exchange_and_move"] = instance["fields"][
+        "q1_2021_exchange_and_move_hub"
+    ]
+    q2_2021_fields["exchange_and_move"] = instance["fields"][
+        "q2_2021_exchange_and_move_hub"
+    ]
+    q3_2021_fields["exchange_and_move"] = instance["fields"][
+        "q3_2021_exchange_and_move_hub"
+    ]
+    q4_2021_fields["exchange_and_move"] = instance["fields"][
+        "q4_2021_exchange_and_move_hub"
+    ]
+
+    # End Update fields
+
+    # Add new fields
+
+    q1_2021_fields["created_by"] = "Admin"
+    q1_2021_fields["created"] = "2000-01-13T13:13:13.000Z"
+    q1_2021_fields["updated_by"] = "Admin"
+    q1_2021_fields["updated"] = "2000-01-13T13:13:13.000Z"
+
+    q2_2021_fields["created_by"] = "Admin"
+    q2_2021_fields["created"] = "2000-01-13T13:13:13.000Z"
+    q2_2021_fields["updated_by"] = "Admin"
+    q2_2021_fields["updated"] = "2000-01-13T13:13:13.000Z"
+
+    q3_2021_fields["created_by"] = "Admin"
+    q3_2021_fields["created"] = "2000-01-13T13:13:13.000Z"
+    q3_2021_fields["updated_by"] = "Admin"
+    q3_2021_fields["updated"] = "2000-01-13T13:13:13.000Z"
+
+    q4_2021_fields["created_by"] = "Admin"
+    q4_2021_fields["created"] = "2000-01-13T13:13:13.000Z"
+    q4_2021_fields["updated_by"] = "Admin"
+    q4_2021_fields["updated"] = "2000-01-13T13:13:13.000Z"
+
+    hub_targets_year_fields["created_by"] = "Admin"
+    hub_targets_year_fields["created"] = "2000-01-13T13:13:13.000Z"
+    hub_targets_year_fields["updated_by"] = "Admin"
+    hub_targets_year_fields["updated"] = "2000-01-13T13:13:13.000Z"
+
+    # End add new fields
+
+    # Add fields to larger dict
+
+    q1_2021["fields"] = q1_2021_fields
+    q2_2021["fields"] = q2_2021_fields
+    q3_2021["fields"] = q3_2021_fields
+    q4_2021["fields"] = q4_2021_fields
+
+    hub_targets_year["fields"] = hub_targets_year_fields
+
+    # End adding fields to larger dict
+
+    # Add to dict
+
+    hub_targets.append(q1_2021)
+    hub_targets.append(q2_2021)
+    hub_targets.append(q3_2021)
+    hub_targets.append(q4_2021)
+    hub_targets.append(hub_targets_year)
+
+hub_targets_dict = hub_targets
+
+with open(
+    "/workspace/laurels-staff/common/data_dump/hubtargets.json", "w"
+) as json_data:
+    json.dump(hub_targets, json_data)
 
 # ----------------------------------------
 # USER PREP
@@ -341,14 +555,20 @@ for instance in profile_model:
 
     # End link many to many hubs to new PK
 
-    # Edit field name
+    # Edit field names
 
     instance["fields"]["employee_targets"] = instance["fields"][
         "target_employee"
     ]
 
+    instance["fields"]["personal_comm"] = instance["fields"]["p_comm"]
+
+    instance["fields"]["office_comm"] = instance["fields"]["o_comm"]
+
     del instance["fields"]["target_employee"]
     del instance["fields"]["target_link"]
+    del instance["fields"]["p_comm"]
+    del instance["fields"]["o_comm"]
 
     # End edit field name
 
@@ -380,11 +600,40 @@ with open(
 
 
 # ----------------------------------------
-# PROPERTY LINK MODEL
+# TOTP MODEL
 # ----------------------------------------
 
 with open(
-    "/workspace/laurels-staff/common/data_dump/originals/property_process.json", "r"
+    "/workspace/laurels-staff/common/data_dump/originals/totp.json",
+    "r",
+) as json_data:
+    totp_model = json.load(json_data)
+
+for instance in totp_model:
+
+    # Loop profile list for employee link & delete old field
+
+    for user_instance in user_dict:
+        if user_instance["old_pk"] == instance["fields"]["user"]:
+            instance["fields"]["user"] = user_instance["pk"]
+
+    # End loop
+
+totp_dict = totp_model
+
+with open(
+    "/workspace/laurels-staff/common/data_dump/totp.json", "w"
+) as json_data:
+    json.dump(totp_model, json_data)
+
+
+# ----------------------------------------
+# PROPERTY PROCESS MODEL
+# ----------------------------------------
+
+with open(
+    "/workspace/laurels-staff/common/data_dump/originals/property_process.json",
+    "r",
 ) as json_data:
     property_link_model = json.load(json_data)
 
@@ -392,29 +641,29 @@ for instance in property_link_model:
 
     # Changing the model to new value
 
-    instance["model"] = "properties.propertylink"
+    instance["model"] = "properties.propertyprocess"
 
     # End changing the model to new value
 
     # Changing the macro_status to new values
 
     if instance["fields"]["macro_status"] == "Valuation":
-        instance["fields"]["macro_status"] = "valuation"
+        instance["fields"]["macro_status"] = "val"
 
     elif instance["fields"]["macro_status"] == "Instruction":
-        instance["fields"]["macro_status"] = "instruction"
+        instance["fields"]["macro_status"] = "inst"
 
     elif instance["fields"]["macro_status"] == "Viewing":
-        instance["fields"]["macro_status"] = "viewing"
+        instance["fields"]["macro_status"] = "view"
 
     elif instance["fields"]["macro_status"] == "Deal":
         instance["fields"]["macro_status"] = "deal"
 
     elif instance["fields"]["macro_status"] == "Complete":
-        instance["fields"]["macro_status"] = "complete"
+        instance["fields"]["macro_status"] = "comp"
 
     elif instance["fields"]["macro_status"] == "Withdrawn":
-        instance["fields"]["macro_status"] = "withdrawn"
+        instance["fields"]["macro_status"] = "withd"
 
     # End change macro_status
 
@@ -425,7 +674,7 @@ for instance in property_link_model:
 
     elif instance["fields"]["sector"] == "Lettings":
         instance["fields"]["sector"] = "lettings"
-    
+
     # End sector change
 
     # Loop property list for property link & delete old field
@@ -491,7 +740,13 @@ with open(
 
 master_dict = []
 
+for object in setup_dict:
+    master_dict.append(object)
+
 for object in hub_dict:
+    master_dict.append(object)
+
+for object in hub_targets_dict:
     master_dict.append(object)
 
 for object in user_dict:
@@ -500,10 +755,16 @@ for object in user_dict:
 for object in profile_dict:
     master_dict.append(object)
 
+for object in totp_dict:
+    master_dict.append(object)
+
 for object in property_dict:
     master_dict.append(object)
 
 for object in propertylink_dict:
+    master_dict.append(object)
+
+for object in super_dict:
     master_dict.append(object)
 
 with open(
