00da3ce963fc25e1af7a98533d568ef998b55417
Tmuat <Thomas_Muat@hotmail.com>
2021-05-21 16:15:47 +0000

    PROPERTIES: Created base property list view

--- properties/urls.py
+++ properties/urls.py
@@ -1,5 +1,7 @@
-from django.urls import path, include
+from django.urls import path
+
+from properties.views import property_list
 
 
 app_name = "properties"
-urlpatterns = []
+urlpatterns = [path("properties/", property_list, name="property_list")]
--- properties/views.py
+++ properties/views.py
@@ -1,3 +1,28 @@
+from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 from django.shortcuts import render
 
-# Create your views here.
+from properties.models import PropertyProcess
+
+
+def property_list(request):
+
+    properties_list = PropertyProcess.objects.all()
+
+    page = request.GET.get('page', 1)
+
+    paginator = Paginator(properties_list, 10)
+    try:
+        properties = paginator.page(page)
+    except PageNotAnInteger:
+        properties = paginator.page(1)
+    except EmptyPage:
+        properties = paginator.page(paginator.num_pages)
+
+    context = {
+        "properties": properties,
+    }
+
+    template = "properties/property_list.html"
+
+    return render(request, template, context)
+
