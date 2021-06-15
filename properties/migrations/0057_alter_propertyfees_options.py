7600ffc28c40eab309616019a5730edc6058c5df
Tmuat <Thomas_Muat@hotmail.com>
2021-06-15 15:58:11 +0000

    CORE: Black

--- properties/migrations/0057_alter_propertyfees_options.py
+++ properties/migrations/0057_alter_propertyfees_options.py
@@ -6,12 +6,20 @@ from django.db import migrations
 class Migration(migrations.Migration):
 
     dependencies = [
-        ('properties', '0056_propertyprocess_furthest_status'),
+        ("properties", "0056_propertyprocess_furthest_status"),
     ]
 
     operations = [
         migrations.AlterModelOptions(
-            name='propertyfees',
-            options={'ordering': ['propertyprocess__property__postcode', 'propertyprocess__property__address_line_1', '-date'], 'verbose_name': 'Property Fee', 'verbose_name_plural': 'Property Fees'},
+            name="propertyfees",
+            options={
+                "ordering": [
+                    "propertyprocess__property__postcode",
+                    "propertyprocess__property__address_line_1",
+                    "-date",
+                ],
+                "verbose_name": "Property Fee",
+                "verbose_name_plural": "Property Fees",
+            },
         ),
     ]
