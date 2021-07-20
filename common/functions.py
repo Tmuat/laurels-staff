ee7550717a089f05b92d3c7b5ed8d11410a25217
Tmuat <Thomas_Muat@hotmail.com>
2021-07-20 10:24:16 +0000

    Added top performers to index page

--- common/functions.py
+++ common/functions.py
@@ -38,9 +38,54 @@ def quarter_and_year_calc(date):
 
     start_month = int(calculate_start_month(date))
     start_year = int(calculate_start_year(date))
-    company_year= start_year
+    company_year = start_year
 
     if start_month == 10:
+        company_year += 1
+
+    if start_month == 10:
+        quarter = "q1"
+    elif start_month == 1:
+        quarter = "q2"
+    elif start_month == 4:
+        quarter = "q3"
+    elif start_month == 7:
+        quarter = "q4"
+
+    end_month = start_month + 2
+
+    data["start_year"] = start_year
+    data["company_year"] = company_year
+    data["start_month"] = start_month
+    data["end_month"] = end_month
+    data["quarter"] = quarter
+
+    return data
+
+
+def last_quarter_and_year_calc(date):
+
+    data = dict()
+
+    def calculate_start_month(date):
+        start_month = (((date.month - 1) // 3) * 3) + 1
+        return start_month
+
+    def calculate_start_year(date):
+        start_year = date.year
+        return start_year
+
+    actual_month = int(calculate_start_month(date))
+    start_year = int(calculate_start_year(date))
+    company_year = start_year
+
+    if actual_month == 1:
+        start_month = 10
+        start_year = start_year - 1
+    else:
+        start_month = actual_month - 3
+
+    if actual_month == 10:
         company_year += 1
 
     if start_month == 10:
--- home/views.py
+++ home/views.py
@@ -7,12 +7,181 @@ from django.db.models import Count, Sum
 from django.shortcuts import render
 from django.utils import timezone
 
-from common.functions import quarter_and_year_calc
+from common.functions import quarter_and_year_calc, last_quarter_and_year_calc
 from properties.models import (
     Offer, PropertyFees, Instruction, Reduction, Valuation
 )
 from regionandhub.models import Hub
 from users.models import Profile, UserTargets
+
+
+def top_performers(date):
+    """
+    Returns last quarters top performers.
+    """
+
+    data = dict()
+
+    last_quarter_and_year = last_quarter_and_year_calc(
+        date
+    )
+
+    year = last_quarter_and_year["start_year"]
+    start_month = last_quarter_and_year["start_month"]
+    end_month = last_quarter_and_year["end_month"]
+    quarter = last_quarter_and_year["quarter"]
+    company_year = last_quarter_and_year["company_year"]
+
+    employee = "propertyprocess__employee__id"
+    link_to_employee = 'propertyprocess__employee'
+    reductions = 'propertyprocess__employee'
+
+    valuations = Valuation.objects.values("valuer") \
+        .annotate(valuation_count=Count(link_to_employee)) \
+        .filter(date__iso_year=year,
+                date__month__gte=start_month,
+                date__month__lte=end_month,
+                propertyprocess__employee__employee_targets=True,
+                propertyprocess__employee__user__is_active=True) \
+        .order_by('-valuation_count')
+
+    instructions = Instruction.objects.values(employee) \
+        .annotate(instruction_count=Count(link_to_employee)) \
+        .filter(date__iso_year=year,
+                date__month__gte=start_month,
+                date__month__lte=end_month,
+                propertyprocess__employee__employee_targets=True,
+                propertyprocess__employee__user__is_active=True) \
+        .order_by('-instruction_count')
+
+    reductions = Reduction.objects.values(employee) \
+        .annotate(reduction_count=Count(link_to_employee)) \
+        .filter(date__iso_year=year,
+                date__month__gte=start_month,
+                date__month__lte=end_month,
+                propertyprocess__employee__employee_targets=True,
+                propertyprocess__employee__user__is_active=True) \
+        .order_by('-reduction_count')
+
+    new_business = PropertyFees.objects.values(employee) \
+        .annotate(new_business_sum=Sum('new_business')) \
+        .filter(date__iso_year=year,
+                date__month__gte=start_month,
+                date__month__lte=end_month,
+                propertyprocess__employee__employee_targets=True,
+                propertyprocess__employee__user__is_active=True,
+                active=True,
+                show_all=True) \
+        .order_by('-new_business_sum')
+
+    targets = UserTargets.objects.filter(
+        year=company_year,
+        quarter=quarter
+    )
+
+    # Renaming fields in querysets
+    for instance in valuations:
+        instance["employee_id"] = instance[
+            "valuer"
+        ]
+        del instance["valuer"]
+
+        instance["valuation_target"] = 0
+
+        for target in targets:
+            if instance["employee_id"] == target.profile_targets.id:
+                try:
+                    valuation_target = round(instance[
+                        "valuation_count"
+                    ] / target.valuations, 2) * 100
+                except ZeroDivisionError:
+                    valuation_target = 0
+                instance["valuation_target"] = valuation_target
+
+    for instance in instructions:
+        instance["employee_id"] = instance[
+            "propertyprocess__employee__id"
+        ]
+        del instance["propertyprocess__employee__id"]
+
+        instance["instruction_target"] = 0
+
+        for target in targets:
+            if instance["employee_id"] == target.profile_targets.id:
+                try:
+                    instruction_target = round(instance[
+                        "instruction_count"
+                    ] / target.instructions, 2) * 100
+                except ZeroDivisionError:
+                    instruction_target = 0
+                instance["instruction_target"] = instruction_target
+
+    for instance in reductions:
+        instance["employee_id"] = instance[
+            "propertyprocess__employee__id"
+        ]
+        del instance["propertyprocess__employee__id"]
+
+        instance["reduction_target"] = 0
+
+        for target in targets:
+            if instance["employee_id"] == target.profile_targets.id:
+                try:
+                    reduction_target = round(instance[
+                        "reduction_count"
+                    ] / target.reductions, 2) * 100
+                except ZeroDivisionError:
+                    reduction_target = 0
+                instance["reduction_target"] = reduction_target
+
+    for instance in new_business:
+        instance["employee_id"] = instance[
+            "propertyprocess__employee__id"
+        ]
+        del instance["propertyprocess__employee__id"]
+
+        instance["new_business_target"] = 0
+
+        for target in targets:
+            if instance["employee_id"] == target.profile_targets.id:
+                try:
+                    new_business_target = round(instance[
+                        "new_business_sum"
+                    ] / target.new_business, 2) * 100
+                except ZeroDivisionError:
+                    new_business_target = 0
+                instance["new_business_target"] = new_business_target
+
+    valuations = sorted(
+        valuations,
+        key=lambda k: k['valuation_target'],
+        reverse=True
+    )
+
+    instructions = sorted(
+        instructions,
+        key=lambda k: k['instruction_target'],
+        reverse=True
+    )
+
+    reductions = sorted(
+        reductions,
+        key=lambda k: k['reduction_target'],
+        reverse=True
+    )
+
+    new_business = sorted(
+        new_business,
+        key=lambda k: k['new_business_target'],
+        reverse=True
+    )
+
+    data["valuations"] = valuations[:2]
+    data["instructions"] = instructions[:2]
+    data["reductions"] = reductions[:2]
+    data["new_business"] = new_business[:2]
+
+    return data
 
 
 @otp_required
@@ -25,6 +194,12 @@ def index(request):
     )
 
     quarter_and_year = quarter_and_year_calc(timezone.now())
+    last_quarter_and_year = top_performers(timezone.now())
+
+    last_valuations = last_quarter_and_year["valuations"]
+    last_instructions = last_quarter_and_year["instructions"]
+    last_reductions = last_quarter_and_year["reductions"]
+    last_new_business = last_quarter_and_year["new_business"]
 
     year = quarter_and_year["start_year"]
     start_month = quarter_and_year["start_month"]
@@ -90,9 +265,12 @@ def index(request):
 
         for target in targets:
             if instance["employee_id"] == target.profile_targets.id:
-                valuation_target = round(instance[
-                    "valuation_count"
-                ] / target.valuations, 2) * 100
+                try:
+                    valuation_target = round(instance[
+                        "valuation_count"
+                    ] / target.valuations, 2) * 100
+                except ZeroDivisionError:
+                    valuation_target = 0
                 instance["valuation_target"] = valuation_target
 
     for instance in instructions:
@@ -105,9 +283,12 @@ def index(request):
 
         for target in targets:
             if instance["employee_id"] == target.profile_targets.id:
-                instruction_target = round(instance[
-                    "instruction_count"
-                ] / target.instructions, 2) * 100
+                try:
+                    instruction_target = round(instance[
+                        "instruction_count"
+                    ] / target.instructions, 2) * 100
+                except ZeroDivisionError:
+                    instruction_target = 0
                 instance["instruction_target"] = instruction_target
 
     for instance in reductions:
@@ -120,9 +301,12 @@ def index(request):
 
         for target in targets:
             if instance["employee_id"] == target.profile_targets.id:
-                reduction_target = round(instance[
-                    "reduction_count"
-                ] / target.reductions, 2) * 100
+                try:
+                    reduction_target = round(instance[
+                        "reduction_count"
+                    ] / target.reductions, 2) * 100
+                except ZeroDivisionError:
+                    reduction_target = 0
                 instance["reduction_target"] = reduction_target
 
     for instance in new_business:
@@ -135,9 +319,12 @@ def index(request):
 
         for target in targets:
             if instance["employee_id"] == target.profile_targets.id:
-                new_business_target = round(instance[
-                    "new_business_sum"
-                ] / target.new_business, 2) * 100
+                try:
+                    new_business_target = round(instance[
+                        "new_business_sum"
+                    ] / target.new_business, 2) * 100
+                except ZeroDivisionError:
+                    new_business_target = 0
                 instance["new_business_target"] = new_business_target
 
     valuations = sorted(
@@ -206,6 +393,10 @@ def index(request):
         "no_instruction_employees": no_instruction_employees,
         "no_reduction_employees": no_reduction_employees,
         "no_new_business_employees": no_new_business_employees,
+        "last_valuations": last_valuations,
+        "last_instructions": last_instructions,
+        "last_reductions": last_reductions,
+        "last_new_business": last_new_business,
     }
 
     return render(request, "home/index.html", context)
