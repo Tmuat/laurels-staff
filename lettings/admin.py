9f1eade9ac1645ecc1c0a403b618c7ccb46d4583
Tmuat <Thomas_Muat@hotmail.com>
2021-08-10 10:03:44 +0000

    PROPERTIES: Created renewal models

--- lettings/admin.py
+++ lettings/admin.py
@@ -1,3 +1,42 @@
 from django.contrib import admin
 
-# Register your models here.
+from lettings.models import (
+    ManagedProperties,
+    Renewals
+)
+
+
+class RenewalsAdminInline(admin.TabularInline):
+    model = Renewals
+    readonly_fields = [
+        "created",
+        "created_by",
+        "updated",
+        "updated_by",
+    ]
+
+
+class ManagedPropertiesAdmin(admin.ModelAdmin):
+    list_display = [
+        "__str__",
+        "lettings_service_level",
+        "is_active",
+    ]
+
+    list_filter = ["is_active", "lettings_service_level",]
+
+    readonly_fields = [
+        "updated_by",
+        "updated",
+        "created_by",
+        "created",
+    ]
+
+    def save_model(self, request, obj, form, change):
+        if not obj.pk:
+            obj.created_by = request.user.get_full_name()
+        obj.updated_by = request.user.get_full_name()
+        super().save_model(request, obj, form, change)
+
+
+admin.site.register(ManagedProperties, ManagedPropertiesAdmin)
--- lettings/models.py
+++ lettings/models.py
@@ -1,3 +1,83 @@
 from django.db import models
 
-# Create your models here.
+from common.models import UpdatedAndCreated
+from properties.models import PropertyProcess, InstructionLettingsExtra
+
+
+class ManagedProperties(UpdatedAndCreated):
+    class Meta:
+        ordering = [
+            "propertyprocess__property__postcode",
+            "propertyprocess__property__address_line_1",
+        ]
+        verbose_name = "Managed Property"
+        verbose_name_plural = "Managed Properties"
+
+    BOOL_CHOICES = [(True, "Yes"), (False, "No")]
+
+    SERVICE_LEVEL = [
+        (InstructionLettingsExtra.INTRO, "Intro Only"),
+        (InstructionLettingsExtra.RENTCOLLECT, "Rent Collect"),
+        (InstructionLettingsExtra.FULLYMANAGED, "Fully Managed"),
+    ]
+
+    propertyprocess = models.OneToOneField(
+        PropertyProcess,
+        on_delete=models.CASCADE,
+        related_name="managed_properties",
+    )
+    lettings_service_level = models.CharField(
+        max_length=40, null=True, blank=True, choices=SERVICE_LEVEL
+    )
+    is_active = models.BooleanField(choices=BOOL_CHOICES)
+
+    def __str__(self):
+        if (
+            self.propertyprocess.property.address_line_2 == ""
+            or self.propertyprocess.property.address_line_2 is None
+        ):
+            property_address = "%s, %s" % (
+                self.propertyprocess.property.postcode,
+                self.propertyprocess.property.address_line_1,
+            )
+        else:
+            property_address = "%s, %s, %s" % (
+                self.propertyprocess.property.postcode,
+                self.propertyprocess.property.address_line_1,
+                self.propertyprocess.property.address_line_2,
+            )
+        return property_address
+
+
+class Renewals(UpdatedAndCreated):
+    class Meta:
+        ordering = [
+            "managed_property__propertyprocess__property__postcode",
+            "managed_property__propertyprocess__property__address_line_1",
+        ]
+        verbose_name = "Renewals"
+        verbose_name_plural = "Renewals"
+
+    managed_property = models.ForeignKey(
+        ManagedProperties, on_delete=models.CASCADE, related_name="renewals"
+    )
+    renewed_on = models.DateField(null=True, blank=False)
+    renewal_date = models.DateField(null=False, blank=False)
+
+    def __str__(self):
+        if (
+            self.managed_property.propertyprocess.property.address_line_2 == ""
+            or self.managed_property.propertyprocess.property.address_line_2
+            is None
+        ):
+            property_address = "%s, %s" % (
+                self.managed_property.propertyprocess.property.postcode,
+                self.managed_property.propertyprocess.property.address_line_1,
+            )
+        else:
+            property_address = "%s, %s, %s" % (
+                self.managed_property.propertyprocess.property.postcode,
+                self.managed_property.propertyprocess.property.address_line_1,
+                self.managed_property.propertyprocess.property.address_line_2,
+            )
+        return property_address
