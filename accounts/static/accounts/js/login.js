532967011456fabf0af28cd3d1999837795d9a67
Tmuat <Thomas_Muat@hotmail.com>
2021-08-10 13:36:01 +0000

    Adjusted login JS

--- accounts/static/accounts/js/login.js
+++ accounts/static/accounts/js/login.js
@@ -1,8 +1,9 @@
 $(document).ready(function () {
     $("#show-otp").click(function () {
-        $("#show-otp").addClass("d-none")
-        $("#otp-inputs").removeClass("d-none")
-        $("#submit").removeClass("d-none")
+        $("#show-otp").addClass("d-none");
+        $("#otp-inputs").removeClass("d-none");
+        $("#submit").removeClass("d-none");
+        $("#id_otp_token").val("");
         return false;
     });
 });
