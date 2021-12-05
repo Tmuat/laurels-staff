267207fef05d7bef5b6fc38742f944ce42800f67
Tmuat <Thomas_Muat@hotmail.com>
2021-12-05 11:28:22 +0000

    Added County & Salutation to andlord

--- touts/models.py
+++ touts/models.py
@@ -69,9 +69,11 @@ class Landlord(UpdatedAndCreated):
         verbose_name_plural = "Tout Landlords"
 
     landlord_name = models.CharField(max_length=150, null=False, blank=False)
+    landlord_salutation = models.CharField(max_length=150, null=False, blank=False)
     address_line_1 = models.CharField(max_length=150, null=False)
     address_line_2 = models.CharField(max_length=150, null=True, blank=True)
     town = models.CharField(max_length=100, null=False)
+    county = models.CharField(max_length=100, null=True)
     postcode = models.CharField(max_length=8, null=False)
     landlord_property = models.ForeignKey(
         ToutProperty,
