d4ee7bb1eaa0da3ae1147ef99332552e1162cc75
Tmuat <Thomas_Muat@hotmail.com>
2021-12-05 16:20:28 +0000

    Basic area view

--- touts/views.py
+++ touts/views.py
@@ -2,9 +2,11 @@ from django_otp.decorators import otp_required
 
 from django.contrib.auth.decorators import login_required
 from django.contrib.admin.views.decorators import staff_member_required
+from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 from django.shortcuts import render
 
 from common.decorators import director_required
+from touts.models import Area
 
 
 @director_required
@@ -17,96 +19,36 @@ def area_list(request):
     searching and filtering.
     """
 
-    # properties_list = PropertyProcess.objects.select_related(
-    #     "property",
-    # ).prefetch_related(
-    #     "employee",
-    #     "hub",
-    #     "history",
-    # )
-    # query = None
-    # status = None
-    # sector = None
-    # archive = False
+    area_list = Area.objects.all()
 
-    # if request.GET:
-    #     if "archive" in request.GET:
-    #         archive = request.GET["archive"]
-    #         if archive == "true":
-    #             archive = True
-    #     if "status" in request.GET:
-    #         status = request.GET["status"]
-    #         if status == "potential":
-    #             properties_list = (
-    #                 properties_list.exclude(macro_status=3)
-    #                 .exclude(macro_status=4)
-    #                 .exclude(macro_status=5)
-    #             )
-    #         elif status == "live":
-    #             properties_list = (
-    #                 properties_list.exclude(macro_status=0)
-    #                 .exclude(macro_status=1)
-    #                 .exclude(macro_status=2)
-    #                 .exclude(macro_status=4)
-    #                 .exclude(macro_status=5)
-    #             )
-    #         elif status == "deal":
-    #             properties_list = (
-    #                 properties_list.exclude(macro_status=0)
-    #                 .exclude(macro_status=1)
-    #                 .exclude(macro_status=2)
-    #                 .exclude(macro_status=3)
-    #                 .exclude(macro_status=5)
-    #             )
-    #         elif status == "complete":
-    #             properties_list = (
-    #                 properties_list.exclude(macro_status=0)
-    #                 .exclude(macro_status=1)
-    #                 .exclude(macro_status=2)
-    #                 .exclude(macro_status=3)
-    #                 .exclude(macro_status=4)
-    #             )
-    #     if "sector" in request.GET:
-    #         sector = request.GET["sector"]
-    #         properties_list = properties_list.filter(sector=sector)
-    #     if "query" in request.GET:
-    #         query = request.GET["query"]
-    #         if not query:
-    #             return redirect(reverse("properties:property_list"))
+    active = True
 
-    #         queries = (
-    #             Q(property__postcode__icontains=query)
-    #             | Q(property__address_line_1__icontains=query)
-    #             | Q(property__address_line_2__icontains=query)
-    #         )
-    #         properties_list = properties_list.filter(queries)
+    if request.GET:
+        if "active" in request.GET:
+            active = request.GET["active"]
+            if active == "True":
+                active = True
+            elif active == "False":
+                active = False
 
-    # if archive is False:
-    #     properties_list = properties_list.exclude(macro_status=-1)
+    if active is True:
+        area_list = area_list.filter(is_active=True)
 
-    # properties_list_length = len(properties_list)
+    page = request.GET.get("page", 1)
 
-    # page = request.GET.get("page", 1)
+    paginator = Paginator(area_list, 16)
+    last_page = paginator.num_pages
 
-    # paginator = Paginator(properties_list, 16)
-    # last_page = paginator.num_pages
-
-    # try:
-    #     properties = paginator.page(page)
-    # except PageNotAnInteger:
-    #     properties = paginator.page(1)
-    # except EmptyPage:
-    #     properties = paginator.page(paginator.num_pages)
+    try:
+        area_list = paginator.page(page)
+    except PageNotAnInteger:
+        area_list = paginator.page(1)
+    except EmptyPage:
+        area_list = paginator.page(paginator.num_pages)
 
     context = {
-        # "properties": properties,
-        # "last_page": last_page,
-        # "properties_length": properties_list_length,
-        # "query": query,
-        # "status": status,
-        # "sector": sector,
-        # "filter": filter,
-        # "archive": archive,
+        "area_list": area_list,
+        "last_page": last_page,
     }
 
     template = "touts/area_list.html"
