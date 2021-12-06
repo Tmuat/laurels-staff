03e1e52c58025cf354b03f34077ee2866bc90bab
Tmuat <Thomas_Muat@hotmail.com>
2021-12-06 21:05:57 +0000

    Created area edit form

--- touts/forms.py
+++ touts/forms.py
@@ -24,3 +24,26 @@ class AreaForm(forms.ModelForm):
         for field in self.fields:
             label = f"{labels[field]}"
             self.fields[field].label = label
+
+
+class AreaEditForm(forms.ModelForm):
+    class Meta:
+        model = Area
+        fields = (
+            "area_code",
+            "is_active"
+        )
+
+    def __init__(self, *args, **kwargs):
+        """
+        Add new labels
+        """
+        super().__init__(*args, **kwargs)
+        labels = {
+            "area_code": "Area Code (Press Enter To Check If Unique)",
+            "is_active": "Is Active?"
+        }
+
+        for field in self.fields:
+            label = f"{labels[field]}"
+            self.fields[field].label = label
