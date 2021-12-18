e5f2ba83f0216e4be3737d1e06e1272f697f9f6c
Tmuat <Thomas_Muat@hotmail.com>
2021-12-18 19:37:00 +0000

    Created AddLandlordExistingPropertyForm

--- touts/forms.py
+++ touts/forms.py
@@ -138,11 +138,76 @@ class AddLandlordForm(forms.ModelForm):
         labels = {
             "landlord_name": "Landlord Name",
             "landlord_salutation": "Landlord Salutation",
-            "address_line_1": "Address Line 1",
-            "address_line_2": "Address Line 2",
-            "town": "Town",
-            "county": "County",
-            "postcode": "Postcode"
+            "address_line_1": "LL Address Line 1",
+            "address_line_2": "LL Address Line 2",
+            "town": "LL Town",
+            "county": "LL County",
+            "postcode": "LL Postcode"
+        }
+
+        for field in self.fields:
+            label = f"{labels[field]}"
+            self.fields[field].label = label
+
+
+class AddLandlordExistingPropertyForm(forms.ModelForm):
+    class Meta:
+        model = Landlord
+        fields = (
+            "landlord_name",
+            "landlord_salutation",
+            "address_line_1",
+            "address_line_2",
+            "town",
+            "county",
+            "postcode",
+            "landlord_property"
+        )
+
+    address_line_1 = forms.CharField(
+        widget=forms.TextInput(attrs={'id': 'id_ll_address_line_1'})
+    )
+
+    address_line_2 = forms.CharField(
+        required=False,
+        widget=forms.TextInput(
+            attrs={
+                'id': 'id_ll_address_line_2',
+            }
+        )
+    )
+
+    town = forms.CharField(
+        widget=forms.TextInput(attrs={'id': 'id_ll_town'})
+    )
+
+    county = forms.CharField(
+        required=False,
+        widget=forms.TextInput(
+            attrs={
+                'id': 'id_ll_county'
+            }
+        )
+    )
+
+    postcode = forms.CharField(
+        widget=forms.TextInput(attrs={'id': 'id_ll_postcode'})
+    )
+
+    def __init__(self, *args, **kwargs):
+        """
+        Add new labels
+        """
+        super().__init__(*args, **kwargs)
+        labels = {
+            "landlord_name": "Landlord Name",
+            "landlord_salutation": "Landlord Salutation",
+            "address_line_1": "LL Address Line 1",
+            "address_line_2": "LL Address Line 2",
+            "town": "LL Town",
+            "county": "LL County",
+            "postcode": "LL Postcode",
+            "landlord_property": "Existing Property"
         }
 
         for field in self.fields:
