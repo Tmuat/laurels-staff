0a22c226e2cae3ceb327b59eed507645d480fa1a
Tmuat <Thomas_Muat@hotmail.com>
2022-01-01 16:06:01 +0000

    Adjusted board string length

--- boards/functions.py
+++ boards/functions.py
@@ -2,7 +2,7 @@ import random
 import string
 
 
-def random_string_generator(size=20,
+def random_string_generator(size=10,
                             chars=string.digits):
     return ''.join(random.choice(chars) for _ in range(size))
 
