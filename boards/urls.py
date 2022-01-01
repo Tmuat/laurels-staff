290b85ffee7bc8b63a590b88ebf8317b68efd67d
Tmuat <Thomas_Muat@hotmail.com>
2022-01-01 16:35:48 +0000

    Created Board Modal Controller

--- boards/urls.py
+++ boards/urls.py
@@ -1,5 +1,13 @@
 from django.urls import path
 
+from boards.views import board_modal_controller
+
 
 app_name = "boards"
-urlpatterns = []
+urlpatterns = [
+    path(
+        "boards/controller/<board_id>/",
+        board_modal_controller,
+        name="board_modal_controller"
+    ),
+]
--- boards/views.py
+++ boards/views.py
@@ -2,8 +2,11 @@ import requests
 
 from xml.etree.ElementTree import Element, SubElement, tostring
 
-from django.shortcuts import render
+from django.http import JsonResponse
+from django.shortcuts import get_object_or_404
+from django.template.loader import render_to_string
 
+from boards.models import Boards
 from laurels.settings.base import (
     BOARDS_URL,
     BOARDS_API_KEY,
@@ -74,42 +77,50 @@ def board_types():
     return r
 
 
+# def create_addMovements_xml(instance, form):
+#     """
+#     """
+
+#     xml_payload = Element("movements")
+
+#     movement = SubElement(xml_payload, "movement")
+
+#     branchcode = SubElement(movement, "branchcode")
+#     branchcode.text = BOARDS_COMPANY_KEY
+
+#     propertyref = SubElement(movement, "propertyref")
+#     propertyref.text = ""
+#     vendorname = SubElement(movement, "vendorname")
+#     vendorname.text = "T Muat"
+#     boardtypeid = SubElement(movement, "boardtypeid")
+#     boardtypeid.text = "42462"
+#     movementtypeid = SubElement(movement, "movementtypeid")
+#     movementtypeid.text = "1"
+#     boardstatusid = SubElement(movement, "boardstatusid")
+#     boardstatusid.text = "1"
+#     noofboards = SubElement(movement, "noofboards")
+#     noofboards.text = "1"
+#     houseno = SubElement(movement, "houseno")
+#     houseno.text = "Flat 5"
+#     address1 = SubElement(movement, "address1")
+#     address1.text = "Woodfield Court"
+#     address2 = SubElement(movement, "address2")
+#     address2.text = "Woodfield Avenue"
+#     locality = SubElement(movement, "locality")
+#     locality.text = ""
+#     town = SubElement(movement, "town")
+#     town.text = "Rugby"
+#     county = SubElement(movement, "county")
+#     county.text = "Warwickshire"
+#     postcode = SubElement(movement, "postcode")
+#     postcode.text = "CV22 5HT"
+#     agentnotes = SubElement(movement, "agentnotes")
+#     agentnotes.text = "Please erect to the side of the house."
+
+#     return xml_payload
+
+
 def new_board():
-
-    xml_payload = Element("movements")
-
-    movement = SubElement(xml_payload, "movement")
-
-    branchcode = SubElement(movement, "branchcode")
-    branchcode.text = "TEST172"
-    propertyref = SubElement(movement, "propertyref")
-    propertyref.text = ""
-    vendorname = SubElement(movement, "vendorname")
-    vendorname.text = "T Muat"
-    boardtypeid = SubElement(movement, "boardtypeid")
-    boardtypeid.text = "42462"
-    movementtypeid = SubElement(movement, "movementtypeid")
-    movementtypeid.text = "1"
-    boardstatusid = SubElement(movement, "boardstatusid")
-    boardstatusid.text = "1"
-    noofboards = SubElement(movement, "noofboards")
-    noofboards.text = "1"
-    houseno = SubElement(movement, "houseno")
-    houseno.text = "Flat 5"
-    address1 = SubElement(movement, "address1")
-    address1.text = "Woodfield Court"
-    address2 = SubElement(movement, "address2")
-    address2.text = "Woodfield Avenue"
-    locality = SubElement(movement, "locality")
-    locality.text = ""
-    town = SubElement(movement, "town")
-    town.text = "Rugby"
-    county = SubElement(movement, "county")
-    county.text = "Warwickshire"
-    postcode = SubElement(movement, "postcode")
-    postcode.text = "CV22 5HT"
-    agentnotes = SubElement(movement, "agentnotes")
-    agentnotes.text = "Please erect to the side of the house."
 
     payload = {
         'key': BOARDS_API_KEY,
@@ -131,3 +142,42 @@ def new_board():
 
 # from boards.views import *
 # board_types()
+
+def board_modal_selector(selector):
+    """
+    A function to return the correct modal html
+    template.
+    """
+
+    modals = {
+        "add_new_board": "new_listing.html",
+    }
+
+    modal = modals[selector]
+
+    return modal
+
+
+def board_modal_controller(request, board_id):
+    """
+    Ajax URL for getting the board options modal.
+    """
+
+    data = dict()
+
+    board_instance = get_object_or_404(
+        Boards, id=board_id
+    )
+
+    modal = board_modal_selector("add_new_board")
+
+    context = {
+        "board_instance": board_instance,
+    }
+    data["html_modal"] = render_to_string(
+        f"touts/includes/forms/{modal}",
+        context,
+        request=request,
+    )
+
+    return JsonResponse(data)
