c1d50f44a9642b783d9b04eef2a82f9ca9342935
Tmuat <Thomas_Muat@hotmail.com>
2022-01-01 14:43:27 +0000

    Created new function for new boards

--- boards/views.py
+++ boards/views.py
@@ -73,5 +73,61 @@ def board_types():
 
     return r
 
+
+def new_board():
+
+    xml_payload = Element("movements")
+
+    movement = SubElement(xml_payload, "movement")
+
+    branchcode = SubElement(movement, "branchcode")
+    branchcode.text = "TEST172"
+    propertyref = SubElement(movement, "propertyref")
+    propertyref.text = ""
+    vendorname = SubElement(movement, "vendorname")
+    vendorname.text = "T Muat"
+    boardtypeid = SubElement(movement, "boardtypeid")
+    boardtypeid.text = "42462"
+    movementtypeid = SubElement(movement, "movementtypeid")
+    movementtypeid.text = "1"
+    boardstatusid = SubElement(movement, "boardstatusid")
+    boardstatusid.text = "1"
+    noofboards = SubElement(movement, "noofboards")
+    noofboards.text = "1"
+    houseno = SubElement(movement, "houseno")
+    houseno.text = "Flat 5"
+    address1 = SubElement(movement, "address1")
+    address1.text = "Woodfield Court"
+    address2 = SubElement(movement, "address2")
+    address2.text = "Woodfield Avenue"
+    locality = SubElement(movement, "locality")
+    locality.text = ""
+    town = SubElement(movement, "town")
+    town.text = "Rugby"
+    county = SubElement(movement, "county")
+    county.text = "Warwickshire"
+    postcode = SubElement(movement, "postcode")
+    postcode.text = "CV22 5HT"
+    agentnotes = SubElement(movement, "agentnotes")
+    agentnotes.text = "Please erect to the side of the house."
+
+    payload = {
+        'key': BOARDS_API_KEY,
+        'branchcode': BOARDS_COMPANY_KEY,
+        'overwriteExisting': True,
+        'returnFullDetails': False,
+    }
+
+    r = requests.get(
+        BOARDS_URL + "addMovements",
+        data=tostring(xml_payload),
+        params=payload
+    )
+
+    print(r.text)
+
+    return r
+
+
 # from boards.views import *
 # board_types()
