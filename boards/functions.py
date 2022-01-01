03eaf051a549a0991a44b562e5ce319e61b67ddf
Tmuat <Thomas_Muat@hotmail.com>
2022-01-01 14:08:00 +0000

    Created unique property ref generator

---
+++ boards/functions.py
@@ -0,0 +1,19 @@
+import random
+import string
+
+
+def random_string_generator(size=20,
+                            chars=string.digits):
+    return ''.join(random.choice(chars) for _ in range(size))
+
+
+def unique_property_ref_generator(instance):
+    new_property_ref = random_string_generator()
+
+    Klass = instance.__class__
+
+    qs_exists = Klass.objects.filter(
+                    propertyref=new_property_ref).exists()
+    if qs_exists:
+        return unique_property_ref_generator(instance)
+    return new_property_ref
