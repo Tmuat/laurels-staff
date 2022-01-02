09f4a65937501d39e151184dc8f4fa2a4e59b639
Tmuat <Thomas_Muat@hotmail.com>
2022-01-02 15:14:07 +0000

    Created new boards sales and lettings forms

--- boards/forms.py
+++ boards/forms.py
@@ -1,41 +1,147 @@
 from django import forms
 
 
-class AddNewBoardForm(forms.Form):
+class AddNewBoardSalesForm(forms.Form):
     vendor_name = forms.CharField(
         strip=True,
-        widget=forms.TextInput(
-            attrs={
-                'required': True
-            }
-        ),
+        widget=forms.TextInput(),
         label="Vendor Name"
     )
 
+    houseno = forms.CharField(
+        strip=True,
+        widget=forms.TextInput(),
+        label="House No."
+    )
 
-#     vendorname = SubElement(movement, "vendorname")
-#     vendorname.text = "T Muat"
-#     boardtypeid = SubElement(movement, "boardtypeid")
-#     boardtypeid.text = "42462"
-#     movementtypeid = SubElement(movement, "movementtypeid")
-#     movementtypeid.text = "1"
-#     boardstatusid = SubElement(movement, "boardstatusid")
-#     boardstatusid.text = "1"
-#     noofboards = SubElement(movement, "noofboards")
-#     noofboards.text = "1"
-#     houseno = SubElement(movement, "houseno")
-#     houseno.text = "Flat 5"
-#     address1 = SubElement(movement, "address1")
-#     address1.text = "Woodfield Court"
-#     address2 = SubElement(movement, "address2")
-#     address2.text = "Woodfield Avenue"
-#     locality = SubElement(movement, "locality")
-#     locality.text = ""
-#     town = SubElement(movement, "town")
-#     town.text = "Rugby"
-#     county = SubElement(movement, "county")
-#     county.text = "Warwickshire"
-#     postcode = SubElement(movement, "postcode")
-#     postcode.text = "CV22 5HT"
-#     agentnotes = SubElement(movement, "agentnotes")
-#     agentnotes.text = "Please erect to the side of the house."
+    address1 = forms.CharField(
+        strip=True,
+        widget=forms.TextInput(),
+        label="Address Line 1"
+    )
+
+    address2 = forms.CharField(
+        strip=True,
+        widget=forms.TextInput(),
+        required=False,
+        label="Address Line 2"
+    )
+
+    town = forms.CharField(
+        strip=True,
+        widget=forms.TextInput(),
+        label="Town"
+    )
+
+    county = forms.CharField(
+        strip=True,
+        widget=forms.TextInput(),
+        required=False,
+        label="County"
+    )
+
+    postcode = forms.CharField(
+        strip=True,
+        widget=forms.TextInput(),
+        label="Postcode"
+    )
+
+    agentnotes = forms.CharField(
+        strip=True,
+        widget=forms.Textarea(
+            attrs={
+                "rows": 2,
+            }
+        ),
+        required=False,
+        label="Agent Notes"
+    )
+
+    FOR_SALE = 1
+    SOLD = 3
+    SALE_AGREED = 5
+    UNDER_OFFER = 6
+
+    BOARD_STATUS = (
+        (FOR_SALE, "For Sale"),
+        (SOLD, "Sold"),
+        (SALE_AGREED, "Sale Agreed"),
+        (UNDER_OFFER, "Under Offer"),
+    )
+
+    boardstatusid = forms.ChoiceField(
+        choices=BOARD_STATUS,
+        label="Board Status"
+    )
+
+
+class AddNewBoardLettingsForm(forms.Form):
+    vendor_name = forms.CharField(
+        strip=True,
+        widget=forms.TextInput(),
+        label="Vendor Name"
+    )
+
+    houseno = forms.CharField(
+        strip=True,
+        widget=forms.TextInput(),
+        label="House No."
+    )
+
+    address1 = forms.CharField(
+        strip=True,
+        widget=forms.TextInput(),
+        label="Address Line 1"
+    )
+
+    address2 = forms.CharField(
+        strip=True,
+        widget=forms.TextInput(),
+        required=False,
+        label="Address Line 2"
+    )
+
+    town = forms.CharField(
+        strip=True,
+        widget=forms.TextInput(),
+        label="Town"
+    )
+
+    county = forms.CharField(
+        strip=True,
+        widget=forms.TextInput(),
+        required=False,
+        label="County"
+    )
+
+    postcode = forms.CharField(
+        strip=True,
+        widget=forms.TextInput(),
+        label="Postcode"
+    )
+
+    agentnotes = forms.CharField(
+        strip=True,
+        widget=forms.Textarea(
+            attrs={
+                "rows": 2,
+            }
+        ),
+        required=False,
+        label="Agent Notes"
+    )
+
+    TO_LET = 2
+    LET_BY = 4
+    MANAGED_BY = 7
+
+    BOARD_STATUS = (
+        (TO_LET, "To Let"),
+        (LET_BY, "Let By"),
+        (MANAGED_BY, "Let & Managed By"),
+    )
+
+    boardstatusid = forms.ChoiceField(
+        choices=BOARD_STATUS,
+        label="Board Status"
+    )
