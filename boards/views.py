d8031110c44a44a55426360d220d548f63ee0e73
Tmuat <Thomas_Muat@hotmail.com>
2022-01-03 19:01:18 +0000

    WIP - retrieve board form

--- boards/views.py
+++ boards/views.py
@@ -1,3 +1,4 @@
+import datetime
 from django_otp.decorators import otp_required
 import requests
 from xml.etree.ElementTree import Element, SubElement, tostring, fromstring
@@ -9,6 +10,7 @@ from django.template.loader import render_to_string
 
 from boards.forms import (
     AddNewBoardForm,
+    RetrieveBoardForm
 )
 from boards.models import Boards
 from laurels.settings.base import (
@@ -82,6 +84,90 @@ def new_board(board_instance, instance):
     board_request = requests.post(
         BOARDS_URL + "addMovements",
         data=tostring(xml_payload),
+        params=payload
+    )
+
+    return board_request
+
+
+def retrieve_board_call(board_instance, instance):
+
+    xml_payload = Element("movements")
+    movement = SubElement(xml_payload, "movement")
+
+    branchcode = SubElement(movement, "branchcode")
+    branchcode.text = BOARDS_COMPANY_KEY
+
+    propertyref = SubElement(movement, "propertyref")
+    propertyref.text = str(board_instance.propertyref)
+
+    vendorname = SubElement(movement, "vendorname")
+    vendorname.text = instance.vendor_name
+
+    boardtypeid = SubElement(movement, "boardtypeid")
+    if board_instance.propertyprocess.sector == "sales":
+        boardtypeid.text = "42462"
+    else:
+        boardtypeid.text = "42463"
+
+    movementtypeid = SubElement(movement, "movementtypeid")
+    movementtypeid.text = "1"
+
+    boardstatusid = SubElement(movement, "boardstatusid")
+    boardstatusid.text = str(instance.boardstatusid)
+
+    noofboards = SubElement(movement, "noofboards")
+    noofboards.text = "1"
+
+    houseno = SubElement(movement, "houseno")
+    houseno.text = ""
+    houseno.text = instance.houseno
+
+    address1 = SubElement(movement, "address1")
+    address1.text = instance.address1
+
+    address2 = SubElement(movement, "address2")
+    address2.text = instance.address2
+
+    locality = SubElement(movement, "locality")
+    locality.text = ""
+
+    town = SubElement(movement, "town")
+    town.text = instance.town
+
+    county = SubElement(movement, "county")
+    county.text = instance.county
+
+    postcode = SubElement(movement, "postcode")
+    postcode.text = instance.postcode
+
+    agentnotes = SubElement(movement, "agentnotes")
+    agentnotes.text = instance.agentnotes
+
+    payload = {
+        'key': BOARDS_API_KEY,
+        'branchcode': BOARDS_COMPANY_KEY,
+        'overwriteExisting': True,
+        'returnFullDetails': True,
+    }
+
+    board_request = requests.post(
+        BOARDS_URL + "addMovements",
+        data=tostring(xml_payload),
+        params=payload
+    )
+
+    return board_request
+
+
+def get_next_active_date():
+
+    payload = {
+        'key': BOARDS_API_KEY,
+    }
+
+    board_request = requests.get(
+        BOARDS_URL + "getNextActiveDate",
         params=payload
     )
 
@@ -225,6 +311,73 @@ def board_info(request, board_id):
     }
     data["html_modal"] = render_to_string(
         "boards/includes/board_info.html",
+        context,
+        request=request,
+    )
+
+    return JsonResponse(data)
+
+
+@otp_required
+@login_required
+def retrieve_board(request, board_id):
+    """
+    Ajax URL for retrieving a board to the signmaster api.
+    """
+    data = dict()
+
+    board_instance = get_object_or_404(
+        Boards, id=board_id
+    )
+
+    parsed_xml = parse_xml(get_next_active_date())
+    activate_date = datetime.datetime.strptime(
+        parsed_xml.find("activedate").text, "%Y-%m-%d"
+    ).date()
+
+    if request.method == "POST":
+        form = RetrieveBoardForm(request.POST)
+        if form.is_valid():
+            data["form_is_valid"] = True
+            # instance = form.save(commit=False)
+            # new_board_request = new_board(board_instance, instance)
+            # parsed_xml = parse_xml(new_board_request)
+
+            # if parsed_xml.find("statuscode").text == "0001":
+            #     data["html_board"] = render_to_string(
+            #         "boards/includes/responses/new_board_success.html",
+            #         request=request,
+            #     )
+            #     id = parsed_xml.find(
+            #         "movements"
+            #         ).find(
+            #             "movement"
+            #             ).find(
+            #                 "id"
+            #                 ).text
+            #     board_instance.signmaster_id = id
+            #     board_instance.created_on_signmaster = True
+            #     board_instance.updated_by = request.user.get_full_name()
+            #     board_instance.save()
+            #     instance.updated_by = request.user.get_full_name()
+            #     instance.save()
+        else:
+            data["form_is_valid"] = False
+    else:
+        form = RetrieveBoardForm(
+            initial={
+                "date": activate_date,
+                "activate_date": activate_date
+            },
+        )
+
+    context = {
+        "form": form,
+        "activate_date": activate_date,
+        "board_instance": board_instance,
+    }
+    data["html_modal"] = render_to_string(
+        "boards/includes/retrieve_board.html",
         context,
         request=request,
     )
