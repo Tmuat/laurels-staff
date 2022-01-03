ac9dabfac4304f9ea6c22c222b67ba250e78244d
Tmuat <Thomas_Muat@hotmail.com>
2022-01-03 19:03:27 +0000

    Created submitFormAndLoadBoards function

--- boards/static/boards/js/boards-v1.js
+++ boards/static/boards/js/boards-v1.js
@@ -4,6 +4,30 @@ $(document).ready(function () {
     });
 
     // Deals with the form submission before loading board modal
+    var submitFormAndLoadBoards = function () {
+        $('#modal-overlay').fadeToggle(100);
+        var form = $(this);
+        $.ajax({
+            url: form.attr("action"),
+            data: form.serialize(),
+            type: form.attr("method"),
+            dataType: 'json',
+            success: function (data) {
+                if (data.form_is_valid) {
+                    $("#base-modal").modal("hide");
+                    $("#base-static-modal").modal("show");
+                    $("#base-static-modal .modal-dialog").html(data.html_board);
+                    $("#modal-overlay").fadeToggle(100);
+                } else {
+                    $("#base-modal .modal-dialog").html(data.html_modal);
+                    $("#modal-overlay").fadeToggle(100);
+                }
+            }
+        });
+        return false;
+    };
+
+    // Deals with the form submission before loading board modal
     var submitLargeFormAndLoadBoards = function () {
         $('#modal-overlay').fadeToggle(100);
         var form = $(this);
@@ -44,7 +68,26 @@ $(document).ready(function () {
         return false;
     };
 
-    $("#base-large-modal").on("submit", ".js-submit-form-boards", submitLargeFormAndLoadBoards);
+    var hideBaseAndLoadModal = function () {
+        $('#modal-overlay').fadeToggle(100);
+        $("#base-modal").modal("hide");
+        var instance = $(this);
+        $.ajax({
+            url: instance.attr("data-url"),
+            type: 'get',
+            dataType: 'json',
+            success: function (data) {
+                $("#base-modal").modal("show");
+                $("#base-modal .modal-dialog").html(data.html_modal);
+                $('#modal-overlay').fadeToggle(100);
+            }
+        });
+        return false;
+    };
+
+    $("#base-modal").on("submit", ".js-submit-form-boards", submitFormAndLoadBoards);
+    $("#base-large-modal").on("submit", ".js-submit-large-form-boards", submitLargeFormAndLoadBoards);
+    $("#base-modal").on("click", ".js-hide-base-load-modal", hideBaseAndLoadModal);
     $("#base-modal").on("click", ".js-hide-base-load-large-modal", hideBaseAndLoadLargeModal);
 
 });
