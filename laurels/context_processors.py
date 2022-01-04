579d91528a0cea5babc22c6142e7c9af5b138cae
Tmuat <Thomas_Muat@hotmail.com>
2022-01-04 19:09:02 +0000

    Created GFT context processor

--- laurels/context_processors.py
+++ laurels/context_processors.py
@@ -1,7 +1,14 @@
 from regionandhub.models import Hub
+from properties.models import GlobalFeatureToggles
 
 
 def active_hubs(request):
     return {
         "hubs": Hub.objects.filter(is_active=True),
+    }
+
+
+def global_feature_toggles(request):
+    return {
+        "gft": GlobalFeatureToggles.objects.all().first(),
     }
