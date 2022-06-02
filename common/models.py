9ae16c1dba6254f31a355a95650cf57865211152
Tmuat <Thomas_Muat@hotmail.com>
2022-06-02 16:53:37 +0000

    Created updated abstract model

--- common/models.py
+++ common/models.py
@@ -12,3 +12,12 @@ class UpdatedAndCreated(models.Model):
     updated = models.DateTimeField(auto_now=True)
     created_by = models.CharField(max_length=100, blank=True)
     updated_by = models.CharField(max_length=100, blank=True)
+
+
+class Updated(models.Model):
+    class Meta:
+        abstract = True
+
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    updated = models.DateTimeField(auto_now=True)
+    updated_by = models.CharField(max_length=100, blank=True)
