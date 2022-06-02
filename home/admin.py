7f378eaec208b22c9124fbc4df619d9f5e61593f
Tmuat <Thomas_Muat@hotmail.com>
2022-06-02 18:31:10 +0000

    Created data is changed model

--- home/admin.py
+++ home/admin.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 
-from home.models import LastQuarterLeaderboard
+from home.models import LastQuarterLeaderboard, DataIsChanged
 
 
 class LastQuarterLeaderboardAdmin(admin.ModelAdmin):
@@ -19,3 +19,20 @@ class LastQuarterLeaderboardAdmin(admin.ModelAdmin):
 
 
 admin.site.register(LastQuarterLeaderboard, LastQuarterLeaderboardAdmin)
+
+
+class DataIsChangedAdmin(admin.ModelAdmin):
+    list_display = [
+        "__str__",
+        "is_changed",
+        "updated_by",
+        "updated",
+    ]
+
+
+    def save_model(self, request, obj, form, change):
+        obj.updated_by = request.user.get_full_name()
+        super().save_model(request, obj, form, change)
+
+
+admin.site.register(DataIsChanged, DataIsChangedAdmin)
--- home/models.py
+++ home/models.py
@@ -4,6 +4,32 @@ from common.models import Updated
 from users.models import Profile
 
 
+class DataIsChanged(Updated):
+    class Meta:
+        verbose_name = "Leaderboard Data Changed"
+        verbose_name_plural = "Leaderboard Data Changed"
+    
+    VALUATIONS = "valuations"
+    INSTRUCTIONS = "instructions"
+    REDUCTIONS = "reductions"
+    NEW_BUSINESS = "new_business"
+    LEADERBOARD = "leaderboard"
+
+    TYPE = [
+        (VALUATIONS, "Valuations"),
+        (INSTRUCTIONS, "Instructions"),
+        (REDUCTIONS, "Reductions"),
+        (NEW_BUSINESS, "New Business"),
+        (LEADERBOARD, "Leaderboard")
+    ]
+    
+    type = models.CharField(max_length=40, null=False, choices=TYPE)
+    is_changed = models.BooleanField(default=False, null=False)
+
+    def __str__(self):
+        return type
+
+
 class LastQuarterLeaderboard(Updated):
     class Meta:
         ordering = ["type", "-count"]
