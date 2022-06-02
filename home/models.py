c4b6092e0a37fc9da5b2e95344e10edd4984a1f1
Tmuat <Thomas_Muat@hotmail.com>
2022-06-02 16:43:15 +0000

    Created last season leadboard model

--- home/models.py
+++ home/models.py
@@ -1,3 +1,35 @@
 from django.db import models
 
-# Create your models here.
+from common.models import Updated
+from users.models import Profile
+
+
+class LastQuarterLeaderboard(Updated):
+    class Meta:
+        ordering = ["type", "count"]
+        verbose_name = "Leaderboard"
+        verbose_name_plural = "Leaderboard"
+
+    VALUATIONS = "valuations"
+    INSTRUCTIONS = "instructions"
+    REDUCTIONS = "reductions"
+    NEW_BUSINESS = "new_business"
+
+    TYPE = [
+        (VALUATIONS, "Valuations"),
+        (INSTRUCTIONS, "Instructions"),
+        (REDUCTIONS, "Reductions"),
+        (NEW_BUSINESS, "New Business")
+    ]
+
+    type = models.CharField(max_length=40, null=False, choices=TYPE)
+    employee = models.ForeignKey(Profile, on_delete=models.CASCADE)
+    count = models.IntegerField(null=False)
+    target_percentage = models.IntegerField(null=False)
+
+    def __str__(self):
+        string = "%s (%s)" % (
+                self.employee.abbreviated_name,
+                self.type
+            )
+        return string
