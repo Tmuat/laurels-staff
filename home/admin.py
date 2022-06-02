9d8a2d42247acd0ea02f40f4c7f8e2d37867c00c
Tmuat <Thomas_Muat@hotmail.com>
2022-06-02 16:54:01 +0000

    Added LastQuarterLeaderBoard to model admin

--- home/admin.py
+++ home/admin.py
@@ -1,3 +1,21 @@
 from django.contrib import admin
 
-# Register your models here.
+from home.models import LastQuarterLeaderboard
+
+
+class LastQuarterLeaderboardAdmin(admin.ModelAdmin):
+    list_display = [
+        "__str__",
+        "count",
+        "target_percentage",
+        "updated_by",
+        "updated",
+    ]
+
+
+    def save_model(self, request, obj, form, change):
+        obj.updated_by = request.user.get_full_name()
+        super().save_model(request, obj, form, change)
+
+
+admin.site.register(LastQuarterLeaderboard, LastQuarterLeaderboardAdmin)
